1f6d188df0eabe5ed5a091a842127acbb1f714e0
blazexnz <lost_homie@hotmail.com>
2024-11-12 18:07:59 +1300

    Update service-worker.js

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -17,22 +17,6 @@ self.addEventListener('install', (event) => {
     );
 });
 
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [CACHE_NAME];
-
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (!cacheWhitelist.includes(cacheName)) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
-
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
