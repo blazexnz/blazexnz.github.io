14ebbb9d40946997de3c2eae7d9182a1dd22ff61
blazexnz <lost_homie@hotmail.com>
2025-09-18 12:12:54 +1200

    Add files via upload

---
+++ test/service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'pao-lookup-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/script.js',
+  '/style.css',        // Optional if you have external CSS
+  '/manifest.json',    // Optional PWA manifest
+  '/config.json',      // Optional config file if you use one
+  '/icon-192.png',     // Optional app icons
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) return caches.delete(name);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
