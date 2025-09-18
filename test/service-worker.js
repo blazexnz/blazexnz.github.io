d5b37b285eef6935b2f9e38b00ab1dbd1052c73a
blazexnz <lost_homie@hotmail.com>
2025-09-18 12:27:01 +1200

    Update service-worker.js

--- test/service-worker.js
+++ test/service-worker.js
@@ -1,42 +1,47 @@
-const CACHE_NAME = 'pao-lookup-cache-v1';
+const CACHE_NAME = 'pao-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/script.js',
-  '/manifest.json',
-  '/config.json', 
-  '/icon-512.png'
+    './',
+    './index.html',
+    './script.js',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
 ];
 
+// Install: cache files
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-  self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
 });
 
+// Activate: clean old caches
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames.map(name => {
-          if (name !== CACHE_NAME) return caches.delete(name);
-        })
-      )
-    )
-  );
-  self.clients.claim();
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.map(key => {
+                if (key !== CACHE_NAME) return caches.delete(key);
+            }))
+        )
+    );
+    self.clients.claim();
 });
 
+// Fetch: cache-first, update in background
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const responseClone = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
-});
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            const fetchPromise = fetch(event.request).then(networkResponse => {
+                if (networkResponse && networkResponse.status === 200) {
+                    caches.open(CACHE_NAME).then(cache => {
+                        cache.put(event.request, networkResponse.clone());
+                    });
+                }
+                return networkResponse;
+            }).catch(() => cachedResponse);
 
+            return cachedResponse || fetchPromise;
+        })
+    );
+});
