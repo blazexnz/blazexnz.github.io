00cfd6a29346dd59ad2b7ede80364df665bb885c
blazexnz <lost_homie@hotmail.com>
2025-10-15 16:14:22 +1300

    Update script.js

--- recipe-app/script.js
+++ recipe-app/script.js
@@ -162,7 +162,7 @@ const itemsData = [
         <ul>
           <li>12.5 hrs hours ready to bake, 13.5 hrs ready to eat</li>
           <li>If starter fed yesterday, then 8.5 hrs ready to bake, 9.5 hrs ready to eat</li>
-          <li>The difference between this one and the regular recipe is that this rests for 2 hrs, instead of 1 hr + overnight inthe fridge</li>
+          <li>The difference between this one and the regular recipe is that this rests for 2 hrs, instead of 1 hr + overnight in the fridge</li>
         </ul>
       `
     },
